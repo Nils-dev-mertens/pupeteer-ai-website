71b9f5eb76e942a2d08361d595fc86f6d8144632
Nils-dev-Mertens <nilsm2950@gmail.com>
2024-12-13 04:03:56 +0100

    added the fucntion to see if the server is active. user is able now to send products and websites to the server and get a response back.

--- script.js
+++ script.js
@@ -2,13 +2,59 @@ async function main() {
     const data = await fetch("http://localhost:3000/table");
     const json = await data.json();
     json.forEach(element => {
-        CreateElementDom(element);
+        CreateProduct(element);
     });
+    FetchOptionsProduct();
+    FetchOptionsWebsite();
 }
-function CreateElementDom(element) {
-    document.body.innerHTML += `<h1>${element.ProductName}</h1><p>${element.prijs}</p><p>${element.WebsiteName}</p><a href="${element.URL}" target="_blank">${element.URL}</a>`;
+async function start() {
+try {
+  const response = await fetch('http://localhost:3000');
+  if (response.ok) {
+    main();
+  }
+  else {
+    ShowGetServer();
+  }
+} catch (error) {
+  ShowGetServer();
 }
-main();
+}
+function ShowGetServer() {
+  document.body.innerHTML = "<h1>get the server</h1><button onclick='start()'>search again or use a different server than localhost</button>";
+}
+async function FetchOptionsProduct() {
+    const data = await fetch("http://localhost:3000/product");
+    const json = await data.json();
+    document.getElementById("productinput").innerHTML = "";
+    json.forEach(element => {
+    CreateOptionsProduct(element);
+  });
+}
+function CreateOptionsProduct(value) {
+  document.getElementById("productinput").innerHTML += `<option value="${value.Id}">${value.Naam}</option>`;
+}
+async function FetchOptionsWebsite() {
+  const data = await fetch("http://localhost:3000/Website");
+  const json = await data.json();
+  document.getElementById("websiteinput").innerHTML = "";
+  json.forEach(element => {
+  CreateOptionsWebsite(element);
+});
+}
+function CreateOptionsWebsite(value) {
+document.getElementById("websiteinput").innerHTML += `<option value="${value.Id}">${value.Naam}</option>`;
+}
+function CreateProduct(element) {
+    document.querySelector(".show").innerHTML += `<h1>${element.ProductName}</h1><p>${element.prijs}</p><p>${element.WebsiteName}</p><a href="${element.URL}" target="_blank">${element.URL}</a>`;
+}
+function ClickProduct() {
+  sendproduct(document.querySelector(".product-input .text-input").value);
+}
+function ClickWebsite() {
+  const value = document.querySelector(".website-input .text-input").value;
+  sendwebsite(value);
+}
 function sendproduct(name) {
     const data = {
         "name" : name
@@ -23,6 +69,7 @@ function sendproduct(name) {
         console.log("Response from Server:", response);
         if(response.ok == true){
           console.log("product added to Database");
+          FetchOptionsProduct();
         }
       }).catch(error => {
         console.log("Error from Server:", error);
@@ -32,6 +79,7 @@ function sendwebsite(name) {
     const data = {
         "name" : name
       };
+      console.log(data);
       fetch("http://localhost:3000/website", {
         method: "POST",
         headers: {
@@ -42,8 +90,10 @@ function sendwebsite(name) {
         console.log("Response from Server:", response);
         if(response.ok == true){
           console.log("Website added to Database");
+          FetchOptionsWebsite();
         }
       }).catch(error => {
         console.log("Error from Server:", error);
       });
 }
+start();
